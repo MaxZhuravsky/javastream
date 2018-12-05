21adb198df7aeaf9034bb6d6368e5159d22d6197
Max Zhuravsky <maxzhuravsky2@gmail.com>
2018-12-05 21:17:09 +0200

    added node class

--- src/com/javastream/Main.java
+++ src/com/javastream/Main.java
@@ -5,4 +5,31 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
