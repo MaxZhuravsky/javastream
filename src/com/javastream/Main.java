1df6a78c568d31353205180ab02bf16cbd814d72
Max Zhuravsky <maxzhuravsky2@gmail.com>
2018-12-05 22:09:52 +0200

    init link list + print

--- src/com/javastream/Main.java
+++ src/com/javastream/Main.java
@@ -1,35 +1,78 @@
 package com.javastream;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        LinkList l = new LinkList();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.printLinkList();
+
     }
 
+    public static class LinkList {
 
-    public class Node {
-        int data;
-        Node next;
+        Node head;
 
-        public Node(int data, Node next) {
-            this.data = data;
-            this.next = next;
+
+        public void add(int data) {
+            Node temp = new Node(data);
+            temp.next = head;
+            head = temp;
+        }
+        public void printLinkList() {
+            Node temp = head;
+
+           while(temp != null) {
+               System.out.println(temp.data);
+               temp = temp.next;
+           }
         }
 
-        public int getData() {
-            return data;
-        }
+        public static class Node {
 
-        public void setData(int data) {
-            this.data = data;
-        }
+            private int data;
+            private Node next;
 
-        public Node getNext() {
-            return next;
-        }
 
-        public void setNext(Node next) {
-            this.next = next;
+            public Node() {
+                this.next = null;
+            }
+
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+
+
+            public Node(int data, Node next) {
+                this.data = data;
+                this.next = next;
+            }
+
+            public int getData() {
+                return data;
+            }
+
+            public void setData(int data) {
+                this.data = data;
+            }
+
+            public Node getNext() {
+                return next;
+            }
+
+            public void setNext(Node next) {
+                this.next = next;
+            }
         }
     }
 }
+
